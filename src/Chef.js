a989ab633e153055b98945e57cd92f1fcfb3eb36
Maria DelSignore <maria.ad27@gmail.com>
2021-05-13 17:26:09 -0600

    Begin chef test

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,21 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
+
+  greetCustomer(name, morning) {
+    if (morning === true) {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+
+  createRestaurant(item) {
+    return `Yes, we're serving ${item} today!`;
+  }
+
 }
 
 module.exports = Chef;
